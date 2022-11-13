a6dc3ece801106af51bcdf52b7e2f1b1d2ba2a72
Taras Prystuplyk <1988taras@ukr.net>
2022-11-13 20:13:00 +0200

    finishhwav

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,17 +1,24 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/users");
 
 const { RequestError } = require("../../utils");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
     throw RequestError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  const result = await User.create({ name, email, password: hashPassword });
+
+  const avatarUrl = gravatar.url(email);
+  const result = await User.create({
+    email,
+    password: hashPassword,
+    avatarUrl,
+  });
   res.status(201).json({
     user: {
       email: result.email,
